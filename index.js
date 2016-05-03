5092d2115aca8806a42833e00cb69797d6718c60
Keerthi G <keerthi.g01@sap.com>
2016-05-03 16:02:45 +0530

    adding node module

---
+++ index.js
@@ -0,0 +1,15 @@
+(function () {
+
+  // node.js
+  if (typeof module != "undefined") {
+    module.exports = boilerplate;
+  }
+  // browser
+  if (typeof window != "undefined") {
+    !window.boilerplate && (window.boilerplate = boilerplate);
+  }
+
+  function boilerplate() {
+    return 'boilerplate';
+  }
+}());
